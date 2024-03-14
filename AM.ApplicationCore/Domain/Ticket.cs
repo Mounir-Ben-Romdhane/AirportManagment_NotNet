f1017327727038f94beb5b1d15f77364cccd33eb
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-03-14 12:20:31 +0100

    add fluent new

---
+++ AM.ApplicationCore/Domain/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Ticket
+    {
+        public double Prix { get; set; }
+        public int Seige { get; set; }
+        public bool VIP { get; set; }
+        public  int PassengerFK { get; set; }
+        public virtual Passenger? Passenger { get; set; }
+        public int FlightFK { get; set; }
+        public virtual Flight? Flight { get; set; }
+        public int NumTicket { get; set; }
+
+    }
+}
